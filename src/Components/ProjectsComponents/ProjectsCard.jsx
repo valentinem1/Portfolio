af3fd224f3e25b82e0bb0323c472acc392c296d3
Valentine <valentinemaillard1@gmail.com>
2020-05-06 19:02:53 -0400

    fixed resize issue with project container and added github and demo buttons

--- src/Components/ProjectsComponents/ProjectsCard.jsx
+++ src/Components/ProjectsComponents/ProjectsCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Container, Card, Image, Dimmer, Loader } from 'semantic-ui-react'
+import { Card, Image, Button } from 'semantic-ui-react'
 
 const ProjectsCard = (props) => {
     
-    let { name, description, image, github, demo, live_website } = props.project
-    // console.log(props.project)
-
+    let { name, description, image, github, demo, live_website } = props.project 
     return (
         <div className="project-info-container">
-                <Card className="project-card">
-                    <Image className="project-image" src={image} alt={name} />
-                    <div className="project-info">
-                        <h4 className="project-name">{name}</h4>
-                        <p className="project-description">{description}</p>
-                    </div>
-                </Card>
+            <Card className="project-card">
+                <Image className="project-image" src={image} alt={name} />
+                <div className="project-info">
+                    <h4 className="project-name">{name}</h4>
+                    <p className="project-description">{description}</p>
+                </div>
+            </Card>
+            <div className="project-links-container">
+                <Button className="github-link-btn"><a className="github-btn-text" target="_blank" rel="noopener noreferrer" href={github}>GitHub</a></Button>
+                <Button className="demo-link-btn"><a className="demo-btn-text" target="_blank" rel="noopener noreferrer" href={demo}>Demo</a></Button>
+            </div>
         </div>
     );
 };
