4919f28ba9a35c6f9dbf76667c3d3c71560f98fa
Valentine <valentinemaillard1@gmail.com>
2020-05-08 15:16:39 -0400

    added vertical bar and school cap logo

--- src/Components/ExperiencesComponents/ExperiencesContainer.jsx
+++ src/Components/ExperiencesComponents/ExperiencesContainer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react'
-
+import { Header, Image } from 'semantic-ui-react'
+import schoolCap from '../images/graduation-cap-logo.png'
 const ExperiencesContainer = () => {
     return (
         <div className="experience-container">
             <Header className="experience-header">Experience</Header>
-            <p className="flatiron-student">Software Engineer Student, Flatiron School</p>
+            <div className="school-experience-logo">
+                <p className="flatiron-student">Software Engineer Student, Flatiron School</p>
+                <Image className="school-cap" src={schoolCap} alt="school-hat" />
+            </div>
+            <hr className="vertical-line" />
         </div>
     );
 };
