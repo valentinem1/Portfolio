5740b25973271c45a1b2bc4edffaf32e032c2631
Valentine <valentinemaillard1@gmail.com>
2020-05-11 17:12:05 -0400

    finished styling first experience

--- src/Components/ExperiencesComponents/ExperiencesContainer.jsx
+++ src/Components/ExperiencesComponents/ExperiencesContainer.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Header, Image } from 'semantic-ui-react'
 import schoolCap from '../images/graduation-cap-logo.png'
+import workBag from '../images/work-logo.png'
+
 const ExperiencesContainer = () => {
+
     return (
         <div className="experience-container">
             <Header className="experience-header">Experience</Header>
-            <div className="experience-box">
-                <p className="flatiron-student">Software Engineer Student, Flatiron School</p>
-                <p className="arrow-right"></p>
+            <div>
+                <div className="work-school-experience-container">
+                    <div className="experience-box">
+                        <p className="flatiron-student">Software Engineer Student, Flatiron School</p>
+                        <p className="arrow-right"></p>
+                    </div>
+                    <p className="flatiron-experience-time">Nov 2019 - Mar 2020 Brooklyn, NY</p>
+                </div>
+                <Image className="school-cap" src={schoolCap} alt="school-hat-logo" />
             </div>
-            <Image className="school-cap" src={schoolCap} alt="school-hat" />
-            <hr className="vertical-line" />
+                <hr className="vertical-line" />
+                <div>
+                    <div className="work-school-experience-container">
+                        <div className="experience-box">
+                            <p className="wfm-buyer">Dairy Buyer, Whole Foods Market</p>
+                            <p className="arrow-left"></p>
+                        </div>
+                        <p className="wfm-buyer-experience-time">Jun 2019 - Nov 2019 Brooklyn, NY</p>
+                    </div>
+                    <Image className="work-bag" src={workBag} alt="work-bag-logo" />
+                </div>
         </div>
     );
 };
