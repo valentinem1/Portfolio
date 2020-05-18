816d9c2df061773941326b66f4931742f3b3e53d
Valentine <valentinemaillard1@gmail.com>
2020-05-18 18:58:15 -0400

    deployment setup for heroku

--- src/index.js
+++ src/index.js
@@ -10,3 +10,9 @@ ReactDOM.render(
     </BrowserRouter>,
   document.getElementById('root')
 );
+
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
