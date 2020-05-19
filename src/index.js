b8479655f91bdcabcb65ac001de35bb696ccebd8
Valentine <valentinemaillard1@gmail.com>
2020-05-19 10:37:14 -0400

    deploying on heroku

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
 );
 
 
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+// var express = require('express');
+// var app = express();
+// app.use(express.static(__dirname + '/'));
+// app.listen(process.env.PORT || 8080);
